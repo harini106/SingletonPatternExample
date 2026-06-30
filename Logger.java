1635c80df6dc98f9a20ae7f94d0af12fd17c595d
HARINI T <145822301+harini106@users.noreply.github.com>
2026-06-30 10:22:18 +0530

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,20 @@
+public class Logger {
+
+    // Step 1: Create only one object
+    private static Logger instance = new Logger();
+
+    // Step 2: Private constructor
+    private Logger() {
+        System.out.println("Logger Object Created");
+    }
+
+    // Step 3: Public method to access the object
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    // Logging method
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
